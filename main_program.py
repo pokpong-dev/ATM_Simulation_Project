a2e0688062ab37bac807a857010bddb56403535e
chitsanupongkiw <chitsanupong.kh@kkumail.com>
2025-10-07 10:40:24 +0700

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -3,7 +3,7 @@ from modules import file_handler, auth, account
 def main():
     print("SARAN O.K")
     print("yim")
-    print("title")
+    print("title lover ขุนแพน")
 
 if __name__ == "__main__":
     main()
