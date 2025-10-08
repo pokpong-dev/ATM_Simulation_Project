349ec7c5509a4ae0868b1b73308a7f443eb58988
Pokpong Limpaphan <235639172+pokpong-dev@users.noreply.github.com>
2025-10-08 15:16:26 +0700

    docs: เพิ่มเอกสารประกอบการใช้งาน (docs/) feat: เขียน module file_handler เสร็จละพร้อมลั่น

--- main_program.py
+++ main_program.py
@@ -1,11 +1,9 @@
 from modules import file_handler, auth, account
 
 def main():
-    print("SARAN O.K")
-    print("yim")
-    print("title lover ขุนแพน")
-    #testkub
+    #file_handlerพร้อมใช้งาน
+    #พิมพ์ file_handle. ได้เบย
+    pass
 
 if __name__ == "__main__":
     main()
-file_handler.read_accounts_file(2222,2,3,2,32.3,3)
