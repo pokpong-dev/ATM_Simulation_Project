844d01337226fe6156484240bb9646492a5a386b
Pokpong Limpaphan <235639172+pokpong-dev@users.noreply.github.com>
2025-10-09 07:23:17 +0700

    ลบ parameter ที่ไม่จำเป็นออก

--- modules/account.py
+++ modules/account.py
@@ -1,5 +1,10 @@
 # modules/account.py
 # พวกระบบบัญชี (ยังไม่ทำจริง แค่โครง)
-#asdwqweqewqwes.
+#asdwqweqewqwes..
 
-
+# import file_handler
+# file_handler.append_account_file(
+#     "MR", "Pokpong", "Numberone",
+#     "123-4-56789-0", "655576",
+#     "2025-10-07T21:35:00+07:00"
+# )
