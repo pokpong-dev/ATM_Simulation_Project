3c6e942c2a4b46f347d7928c1d90dfdf58771f09
chitsanupongkiw <chitsanupong.kh@kkumail.com>
2025-10-10 17:49:46 +0700

    pokka

--- modules/auth.py
+++ modules/auth.py
@@ -5,42 +5,39 @@ from datetime import datetime
 def register(title, first_name, last_name, account_number, pin, created_at):
     #title ถ้าไม่ได้รับ MR หรือ ms
     if title != "MR" and title != "MS":
-        return {"status": "failed", "msg": "Select title MR or MS"}
+        return {"status": "Error", "msg": "Select title MR or MS"}
 
     piset_letter = ['@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '}', '[', ']', ':', ';', '"',
                      "'", '<', '>', ',', '.', '?', '/', '\\', '|', '`']
     #เช็คพวกตัวอักษรพิเศษ
     for i in piset_letter:
         if i in first_name:
-            return {"status": "failed", "msg": "First Names cannot contain special characters"}
+            return {"status": "Error", "msg": "ชื่อจริง ใส่ตัวอักษรพิเศษไม่ได้ กรอกเป็นตัวอักษรภาษาอังกฤษเท่านั้น"}
         if i in last_name:
-            return {"status": "failed", "msg": "Last  Names cannot contain special characters"}
+            return {"status": "Error", "msg": "นามสกุล ใส่ตัวอักษรพิเศษไม่ได้ กรอกเป็นตัวอักษรภาษาอังกฤษเท่านั้น"}
 
     #เช็คเลขบัญชีว่าเคยสั่งสมัครยัง ในไฟ
     log_login = file_handler.read_accounts_file()
     for i in range(len(log_login)):
         num_accs = log_login[i].split(",")[3]
         if account_number == num_accs:
-            return {"status": "failed", "msg": "This account number already exists"}
-    piset_letter = [
-        "[", "@", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "{", "}", "\\", "]", ";", ":", "'", '"',
-        ",", ".", "<", ">", "/", "?"
-    ]
+            return {"status": "Error", "msg": "เลขบัญชีนี้มีอยู่แล้ว"}
+
     # เช็ค fname กับ Lname ว่าตัวแรกเป็นตัวใหฐ่ไหม กับเชคว่าเปนตัวเลขป่าว
     if first_name.isdigit() or not first_name[0].isupper():
-        return {"status": "failed", "msg": "First name must start with a capital letter and contain only letters"}
+        return {"status": "Error", "msg": "ชื่อจริง กรอกเป็นตัวอักษร และ ตัวแรกต้องเป็นตัวพิมพ์ใหญ่เท่านั้น"}
     if last_name.isdigit() or not last_name[0].isupper():
-        return {"status": "failed", "msg": "Last name must start with a capital letter and contain only letters"}
+        return {"status": "Error", "msg": "นามสกุล กรอกเป็นตัวอักษร และ ตัวแรกต้องเป็นตัวพิมพ์ใหญ่เท่านั้น"}
 
     #ดักพวกกรอก พิน มักง่าย
     easy_pin = ["123456", "111111", "987654", "000000", "123321", "654321", "222222", "333333", "444444", "555555",
                 "666666", "777777", "888888", "999999"]
     if pin in easy_pin:
-        return {"status": "failed", "msg": "The pin is too simple"}
+        return {"status": "Error", "msg": "รหัสง่ายเกินไป"}
     if len(pin) != 6:
-        return {"status": "failed", "msg": "Please enter 6 digits"}
+        return {"status": "Error", "msg": "กรอก PIN 6 ตัว เท่านั้น"}
     if not pin.isdigit():
-        return {"status": "failed", "msg": "Please enter only numbers"}
+        return {"status": "Error", "msg": "กรอกเป็นตัวเลขเท่านั้น"}
 
     #สุ่มเลขบัญชี
     acc_nums1 = random.randint(0, 999)
@@ -55,7 +52,7 @@ def register(title, first_name, last_name, account_number, pin, created_at):
     #เพิ่มข้อมูลเข้าไฟ
     file_handler.append_account_file(title, first_name, last_name, account_number, pin, created_at)
 
-    return {"status": "success", "msg": "Registration successful"}
+    return {"status": "success", "msg": "สมัครบัญชี สำเร็จ"}
 
 
 def login(account_number, pin,created_at):
@@ -76,16 +73,16 @@ def login(account_number, pin,created_at):
         if account_number == num_accs:
 
             if pin == stored_pin:
-                return {"status": "success", "msg": "Login successful"}
+                return {"status": "success", "msg": "ล็อคอิน สำเร็จ"}
             else:
-                return {"status": "failed", "msg": "Incorrect PIN"}
+                return {"status": "failed", "msg": "PIN ไม่ถูกต้อง"}
 
     #ถ้าไม่เจอเลขบัญชีในไฟ
-    return {"status": "failed", "msg": "Account number not found"}
+    return {"status": "Error", "msg": "ไม่พบบัญชี"}
 
 
 #ทดสอบ สมัคร เฉยๆ
-test_registration = register("MR", "M", "K", "xxx-x-xxxxx-x", "122456", "s")
+test_registration = register("MR", "W", "K#", "xxx-x-xxxxx-x", "122456", "s")
 print(test_registration)
 
 #ทดสอบการล้อคอิน
