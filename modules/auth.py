a1be02abc017c949c92c56d0cac2826813dd5ed4
chitsanupongkiw <chitsanupong.kh@kkumail.com>
2025-10-10 16:10:00 +0700

    ล่าสุเUpdate auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -1,7 +1,7 @@
 import file_handler
 import random
 from datetime import datetime
-#eiei
+
 def register(title, first_name, last_name, account_number, pin, created_at):
     #title ถ้าไม่ได้รับ MR หรือ ms
     if title != "MR" and title != "MS":
